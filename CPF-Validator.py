690af1b226a99c51b2031c9d5a6a8cc6fdefe337
unknown <ian.pinho.tsis@gmail.com>
2023-04-27 15:06:01 -0300

    While criado para verificar se o primeiro dos dois ultimos dígitos é válido ou não.

--- CPF-Validator.py
+++ CPF-Validator.py
@@ -1,7 +1,7 @@
 validade=False
 cpf=(input())
 cpfNovo=[]
-numCresc=['10','9','8','7','6','5','4','3','2']
+numCresc=["10","9","8","7","6","5","4","3","2"]
 numCresc1=['11','10','9','8','7','6','5','4','3','2']
 resultado=[]
 
@@ -9,3 +9,10 @@ for i in cpf:
     valor = i
     cpfNovo.append(valor)
 cpfNovo.pop(3),cpfNovo.pop(6),cpfNovo.pop(9)
+
+while validade==False:
+    resultado = list(map(lambda x,y: int(x)*int(y) ,numCresc,cpfNovo))
+    soma=sum(resultado)
+    resto = soma%int(numCresc1[0])
+    numero1=int(numCresc1[0])-int(resto)
+    print(numero1)
