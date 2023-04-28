ce71f3a4bdb6df58a2e2c24dd9b47a8ed14f1fb1
unknown <ian.pinho.tsis@gmail.com>
2023-04-28 10:00:02 -0300

    Ultima atualização do código, resolvendo o problema do código não validar um cpf válido se você digita um cpf inválido na primeira tentativa.

--- CPF-Validator.py
+++ CPF-Validator.py
@@ -4,29 +4,27 @@ numCresc1=["11","10","9","8","7","6","5","4","3","2"]
 resultado=[]
 print("Digite aqui seu CPF com um '.' a cada 3 números e um '-' antes dos 2 ultimos dígitos.")
 cpf=input()
-for i in cpf:
-    valor = i
-    cpfNovo.append(valor)
-cpfNovo.pop(3),cpfNovo.pop(6),cpfNovo.pop(9)
 
-def resultado (numCresc,cpfNovo):
-    validade=False
-    while validade==False:
-        
-        resultado = list(map(lambda x,y: int(x)*int(y) ,numCresc,cpfNovo))
-        soma=sum(resultado)
-        resto = soma%int(numCresc1[0])
-        numero1=int(numCresc1[0])-int(resto)
-        print(numero1)
-        resultado2 = list(map(lambda x,y: int(x)*int(y) ,numCresc1,cpfNovo))
-        resto2=sum(resultado2)%int(numCresc1[0])
-        numero2=int(numCresc1[0])-int(resto2)
-        print(numero2)
-        if int(numero1) ==int(cpfNovo[9]) and int(numero2) ==int(cpfNovo[10]):
-            print("CPF válido!")
-            validade=True
-        else:
-            print("CPF inválido, por favor digite novamente")
-            cpf=input()
+def resultado (numCresc,cpfNovo,cpf):
+    for i in cpf:
+        valor = i
+        cpfNovo.append(valor)
+    cpfNovo.pop(3),cpfNovo.pop(6),cpfNovo.pop(9)
+    resultado = list(map(lambda x,y: int(x)*int(y) ,numCresc,cpfNovo))
+    soma=sum(resultado)
+    resto = soma%int(numCresc1[0])
+    numero1=int(numCresc1[0])-int(resto)
+    resultado2 = list(map(lambda x,y: int(x)*int(y) ,numCresc1,cpfNovo))
+    resto2=sum(resultado2)%int(numCresc1[0])
+    numero2=int(numCresc1[0])-int(resto2)
+    if int(numero1) ==int(cpfNovo[9]) and int(numero2) ==int(cpfNovo[10]):
+        return True
+    else:
+        return False
 
-resultado(numCresc,cpfNovo)
+while not resultado(numCresc,cpfNovo,cpf):
+    print("CPF inválido, por favor digite novamente")
+    cpf=input()
+    cpfNovo=[]
+    resultado(numCresc,cpfNovo,cpf)
+print("CPF válido!")
